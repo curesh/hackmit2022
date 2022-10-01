d42cbdbdea63fbf6e2a2744e9fd35798c294bd86
“Chandra <Chandra.b.suresh@gmail.com>
2022-10-01 13:50:30 -0400

    Initial commit with map (doesn't work change map style)

---
+++ gardencity/src/App.js
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+import './App.css';
+import HomePageScreen from './components/HomePage';
+
+function App() {
+  return (
+    <HomePageScreen />
+  );
+}
+
+export default App;
