243f0057e3208520753a01b1c7572cd95b78adb9
Avnish Sengupta <avnishsengupta1@g.ucla.edu>
2022-10-02 03:35:36 -0400

    working on landing page

--- gardencity/src/components/LandingPage.js
+++ gardencity/src/components/LandingPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import landingPageDisplay from '../assets/landingPageDisplay.svg';
 function LandingPage(){
     return(
-        <div style={{display:'flex', margin:'auto',placeItems:'center'}}>
-            This is a Landing Page <br/>
-            <a href='http://localhost:3000/map'>Click here to go to the map</a>
-            <a href='http://localhost:3000/sidebarTemp'>Click here to go to the temporary sidebar</a>
+        <div className='landing-page-body'>
+        <p style={{fontFamily:'Manrope',fontWeight:'bold',fontSize:'50px'}}>GardenCity</p>
+        <p>Volunteer to plan in a community garden or list your garden space!</p>
+        {/* <a href='http://localhost:3000/map'>Click here to go to the map</a>
+        <a href='http://localhost:3000/sidebarTemp'>Click here to go to the temporary sidebar</a> */}
+
+        <img src={landingPageDisplay} alt='two flowers-a green one and a pink one'/>
         </div>
     )
 }
